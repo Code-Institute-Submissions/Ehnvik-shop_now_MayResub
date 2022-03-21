422c982c518707cfdb373d0aca5fa273fa8f693e
Ehnvik <gustav.ehnvik@gmail.com>
2022-03-21 11:10:19 +0000

    Added stripe js and Stripe API key

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KahTgC0pwCdMcR6REZj2LbrHUuwZsF99TJ0NpoHxZuRfTt9gttkR4kHoVLELIIPHDKMM0MzS04ttF1QMMjr6GoL0072hV1ZpV',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
