575ddf3c99d9f0aee1a1ef8e17c64885c27bc13e
Ehnvik <gustav.ehnvik@gmail.com>
2022-03-21 22:22:15 +0000

    Removed secret key

--- shop_now/settings.py
+++ shop_now/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2hmvxr1kn512cg!(f(2n9-2j3=vs%gzriup-w1swj48#q@xmel'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ehnvik-shop-now.herokuapp.com', 'localhost']
 
